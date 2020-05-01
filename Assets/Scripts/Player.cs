1bd3a9977fb0fd6767e1bdde35f1cf1810a65219
Laurianelo <32616344+Laurianelo@users.noreply.github.com>
2020-05-01 20:01:22 +0200

    scripts player, bullet

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _bulletPrefab;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_bulletPrefab);//re-use
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/PoolManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    //turn this class into a singleto for easy accessibility
+    private static PoolManager _instance;
+    public static PoolManager Instance
+    {
+        get
+        {
+            if (_instance == null) ;
+            Debug.LogError("The Pool Manager is NULL");
+
+            return _instance;
+        }
+    }
+
+
+    private void Awake()
+    {
+        
+    }
+
+    // pregenerate a list of bullets using the bullet prefab
+
+    List<GameObject> GenerateBullets()
+    {
+        return null;
+    }
+}
